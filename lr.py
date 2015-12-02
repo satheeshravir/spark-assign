d96d90537ee0a47800200ef91ede9f6990de990b
satheeshravir <satheeshravir@gmail.com>
2015-12-01 19:00:01 -0800

    Commited the partition and intercepts

--- lr.py
+++ lr.py
@@ -1,9 +1,9 @@
 import sys
 import os
-SPARK_HOME = "/home/maxsteal/Downloads/spark-1.5.2-bin-hadoop2.6/" # Set this to wherever you have compiled Spark
-os.environ["SPARK_HOME"] = SPARK_HOME # Add Spark path
-os.environ["SPARK_LOCAL_IP"] = "127.0.0.1" # Set Local IP
-sys.path.append( SPARK_HOME + "/python") # Add python files to Python Path
+#SPARK_HOME = "/home/maxsteal/Downloads/spark-1.5.2-bin-hadoop2.6/" # Set this to wherever you have compiled Spark
+#os.environ["SPARK_HOME"] = SPARK_HOME # Add Spark path
+#os.environ["SPARK_LOCAL_IP"] = "127.0.0.1" # Set Local IP
+#sys.path.append( SPARK_HOME + "/python") # Add python files to Python Path
 
 from pyspark.mllib.classification import LogisticRegressionWithSGD
 import numpy as np
@@ -34,17 +34,25 @@ def mapper(line):
     #features = [ float(feature) for feature in feats ] # need floats
     return LabeledPoint(label, feats)
 
+
+trainFile = sys.argv[1];
+partitions = int(sys.argv[3]);
+paramsFile = open(sys.argv[2],'r');
+params = []
+for line in paramsFile:
+    params = line.split("\t")
+
 sc = getSparkContext()
 
+
 # Load and parse the data
-data = sc.textFile("train/study_hours.txt")
+data = sc.textFile(trainFile, partitions)
 
 parsedData = data.map(mapper)
 
-print "########################################"
 
 # Train model
-model = LogisticRegressionWithSGD.train(parsedData)
+model = LogisticRegressionWithSGD.train(parsedData, initialWeights=params)
 
 # Predict the first elem will be actual data and the second 
 # item will be the prediction of the model
@@ -52,7 +60,7 @@ labelsAndPreds = parsedData.map(lambda point: (int(point.label),
         model.predict(point.features)))
 
 # Evaluating the model on training data
-trainErr = labelsAndPreds.filter(lambda (v, p): v != p).count() / float(parsedData.count())
+accuracy = labelsAndPreds.filter(lambda (v, p): v == p).count() / float(parsedData.count())
 
 # Print some stuff
-print("Training Error = " + str(trainErr))
+print("Accuracy: " + str(accuracy))
